cfd7bfe92741371195cd65fa288c1a81548ecaa6
chhhhhiikun <chhhhhiikun@gmail.com>
2026-05-18 00:18:51 +0900

    草木にんじん収穫機を追加

---
+++ harvest_tree_carrot.py
@@ -0,0 +1,25 @@
+import utl
+clear()
+
+w = get_world_size()
+
+# (x=奇数 and y=奇数) or (x=偶数 and y=偶数)のときに木を植える
+for i in range(w / 2):
+	for i in range(w):
+		if (not utl.is_even(get_pos_x()) and not utl.is_even(get_pos_y())) or utl.is_even(get_pos_x()) and utl.is_even(get_pos_y()):
+			plant(Entities.Tree)
+		else:
+			till()
+			plant(Entities.Carrot)
+		utl.ez_move("r")
+	utl.ez_move("u")	
+ 
+for i in range(w / 2):
+	for i in range(w):
+		if (not utl.is_even(get_pos_x()) and not utl.is_even(get_pos_y())) or utl.is_even(get_pos_x()) and utl.is_even(get_pos_y()):
+			plant(Entities.Tree)
+		utl.ez_move("r")
+	utl.ez_move("u")	
+
+while True:
+	utl.harvest_plant_and_move_v2()
